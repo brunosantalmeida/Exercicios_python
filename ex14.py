195a813d07094239db8ef056e441ca4d8fc03dab
brunosantalmeida <ornube8080@gmail.com>
2024-07-18 15:53:14 -0300

    conversor de temperatura e aluguel de carro

---
+++ ex14.py
@@ -0,0 +1,6 @@
+# aluguel de carro 
+
+dias = int(input('Quantos dias alugados?'))
+km = float(input('Quantos km rodados?'))
+pago = dias * 60 + (km * 0.15)
+print(' O  total a pagar é de R${:.2f}'.format(pago))
