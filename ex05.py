93fcff9158c15438ae51b24338b8480c90fef852
brunosantalmeida <ornube8080@gmail.com>
2024-07-18 15:22:11 -0300

    operaçoes com numeros

---
+++ ex05.py
@@ -0,0 +1,4 @@
+# antecessor e sucessor
+
+n = int(input('Digite um numero'))
+print( 'Analizando o seu valor {} e seu antessor {} e o sucessor {}'.format(n,(n-1),(n+2)))
---
+++ ex06.py
@@ -0,0 +1,18 @@
+# calculo do dobro, do triplo e da raiz quadrada de um numero 
+
+n = int(input('Digite um numero: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O doblo de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}\n.A raiz quadrada de  {} e igual a {:.2f}.'.format(n, t, n, r))
+
+# outra forma de fazer o codigo
+
+# n = int(input('Digite um numero: '))
+# print('O dobro de {} vale {}.'.format(n*2 ))
+# print('O triplo de {} vale {}\n.A raiz quadrada de  {} e igual a {}.'.format(n, (n*3), n, (n**(1/2))))
+# 
+#  obs: \n quebra de linha 
+# 
+
