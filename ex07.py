1a7918f21406b3d17de4e42a0072f72e11bc54cb
brunosantalmeida <ornube8080@gmail.com>
2024-07-18 15:36:48 -0300

    media e conversor

---
+++ ex07.py
@@ -0,0 +1,8 @@
+# media do aluno 
+
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segundo nota do aluno: '))
+media = (n1 + n2) /2
+print('A media entre {} e {} é igual a {}'.format(n1, n2, media))
+
+# outra forma de applicaçao e acrecenta entre as chaves {} adiciocna :.1f= {:.1f}
