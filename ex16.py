2523e01e96b35b5f3e87dbd469ddc73a2c8a8bbc
brunosantalmeida <ornube8080@gmail.com>
2024-07-18 15:58:50 -0300

    modulos e quebrando numeros

---
+++ ex16.py
@@ -0,0 +1,10 @@
+# quebrando um numero
+
+'''from math import trunc
+num = float(input('Digite um valor'))
+print(' O valor digitado foi {} e a sua porçao inteira e {}'.format(num, trunc(num)))'''
+
+num = float(input('Digite um valor:'))
+print('O valor digitado foi {} e a sua porçao interia e {}'.format(num, int(num)))
+
+# duas formas de escrever o codigo obs: tanto as aspas como a Cerquilha serve para comenta # / '''
